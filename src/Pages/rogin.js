6b5e03154138b2824f6cdbc8e05cd7c585a057ff
우엥 <105274015+rnldudns@users.noreply.github.com>
2023-05-30 21:10:44 +0900

    env작업을 통한 apikey 보호

--- src/Pages/rogin.js
+++ src/Pages/rogin.js
@@ -3,9 +3,7 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 function Main() {
-    const clientId="4268ba164e2c473fb6ec04b4283983a159707d3a531342c0a61d1c427f6b7cec"
-    const BaseLoginUrl="https://gauth.co.kr/login";
-    const redirect_uri="http://localhost:3000/main"
+    
   const onClick = () => {
     window.open(`${BaseLoginUrl}?client_id=${clientId}&redirect_uri=${redirect_uri}`)
   }
