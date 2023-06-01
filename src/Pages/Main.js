652846731a5237ac2878af7caff57ae5e83fed86
우엥 <105274015+rnldudns@users.noreply.github.com>
2023-06-01 21:48:02 +0900

    loginPage add base url

--- src/Pages/Main.js
+++ src/Pages/Main.js
@@ -3,6 +3,7 @@ import { useState } from "react";
 import { useSearchParams } from "react-router-dom";
 
 function Main() {
+  
   const [searchParams, setSearchParams] = useSearchParams();
   console.log(searchParams.get("code"))
   const onClick = () => {
--- src/Pages/rogin.js
+++ src/Pages/rogin.js
@@ -3,9 +3,11 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 function Main() {
-    
+  const BaseLoginUrl = process.env.REACT_APP_BaseLoginUrl
+  const clientId = process.env.REACT_APP_CLIENT_ID
+  const redirect_uri = process.env.REACT_APP_redirect_uri
   const onClick = () => {
-    window.open(`${BaseLoginUrl}?client_id=${clientId}&redirect_uri=${redirect_uri}`)
+    window.open(`${BaseLoginUrl}?client_id=${clientId}&redirect_uri=${redirect_uri}`,"_self")
   }
   return (
     <div>
