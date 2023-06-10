e66acf330addd747bc0a742af41d03a856340cb3
우엥 <105274015+rnldudns@users.noreply.github.com>
2023-06-10 23:59:02 +0900

    Create EnvConfig.js

---
+++ src/api/EnvConfig.js
@@ -0,0 +1,9 @@
+const EnvConfig = {
+    CLIENT_ID:process.env.REACT_APP_CLIENT_ID,
+    BASE_LOGIN_URL:process.env.REACT_APP_BASE_LOGIN_URL,
+    redirect_uri:process.env.REACT_APP_REDIRECT_URI,
+    clientSecret:process.env.REACT_APP_CLIENT_SECRET,
+
+    
+}
+export default EnvConfig;
